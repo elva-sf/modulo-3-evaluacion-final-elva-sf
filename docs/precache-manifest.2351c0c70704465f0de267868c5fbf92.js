8553afeb3f750c712be80bdc90f83c2a5e795040
Elva Souto Fernández <e.souto.fernandez@gmail.com>
2020-01-13 20:15:55 +0100

    add build/docs folder

---
+++ docs/precache-manifest.2351c0c70704465f0de267868c5fbf92.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e507cc830424159102f2c81aa82cc6e2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "81bbd80934ce831a5778",
+    "url": "./static/css/main.b89b6382.chunk.css"
+  },
+  {
+    "revision": "83ea652184326035c3c2",
+    "url": "./static/js/2.3bf58483.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.3bf58483.chunk.js.LICENSE"
+  },
+  {
+    "revision": "81bbd80934ce831a5778",
+    "url": "./static/js/main.a7b8c033.chunk.js"
+  },
+  {
+    "revision": "22284c4c3ec29ce2b17f",
+    "url": "./static/js/runtime-main.6e8bf2de.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/Rick_and_Morty_-_logo_(English).75509f2e.png"
+  }
+]);
